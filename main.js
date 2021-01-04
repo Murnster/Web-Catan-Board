f8fc155d93335d3b3486d5916bcbe8a38ae85885
Ryan Murney <rmurney@gmail.com>
2021-01-03 23:02:11 -0400

    Initial steps - RM

---
+++ main.js
@@ -0,0 +1,25 @@
+// Author: Ryan Murney
+// Main javascript file for Catan Board Generator Application
+
+// Numbers on the board
+boardNums = [2,12,3,3,11,11,4,4,10,10,5,5,9,9,6,6,8,8]
+
+// Terrains on the board in the form of numbers
+// 1 - Wood, 2 - Sheep, 3 - Wheat, 4 - Clay, 5 - Rocks, 6 - Desert
+terrains = [1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,5,5,5,6]
+
+// Ports on the board in the form of numbers
+// 1 - 3:1, 2 - Wood, 3 - Sheep, 4 - Wheat, 5 - Clay, 6 - Rocks
+ports = [1,1,1,1,2,3,4,5,6]
+
+// Generates the each number by using the numGen() function then places them
+// together to form the catan board. Checks numbers at the end to ensure rules
+// of generation.
+function boardGen() {
+
+}
+
+// Randomly selects number based off places next to eachother
+function numGen(one, two, three, four, five, six) {
+
+}
